8fcacdab7d41327f147dfc6843ac27ab28c753ed
ekakshjanweja <jekaksh@gmail.com>
2025-11-20 00:02:05 +0530

    fix: loadThemeMode

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import "package:flutter/material.dart";
 import "package:theme_tut/app.dart";
 import "package:theme_tut/local_storage/local_storage.dart";
+import "package:theme_tut/theme/theme_notifier.dart";
 
 void main(List<String> args) async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -10,4 +11,5 @@ void main(List<String> args) async {
 
 Future<void> bootStrap() async {
   await LocalStorage.init();
+  await ThemeNotifier().loadThemeMode();
 }
